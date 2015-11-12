3d99fa4186ac419d810fa160a366e4274b854c82
Michel Schep <mschep@deloitte.nl>
2015-11-12 11:20:44 +0100

    Basic deserialization of json person

--- VersioningExplorer/VersioningExplorer/Class1.cs
+++ VersioningExplorer/VersioningExplorer/Class1.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NUnit.Framework;
 
 namespace VersioningExplorer
 {
+    [TestFixture]
     public class Class1
     {
+        [Test]
+        public void test()
+        {
+            var serializedMessageV1 = "{'name': 'rob'}";
+
+            var serializer = new OurSerializer();
+
+            var deserializedMessageV1 = serializer.Deserialize(serializedMessageV1);
+
+            Assert.That(deserializedMessageV1.Name, Is.EqualTo("rob"), "name of person");
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+
+    public class OurSerializer
+    {
+        public Person Deserialize(string serializedMessageV1)
+        {
+            return JsonConvert.DeserializeObject<Person>(serializedMessageV1);
+        }
     }
 }
